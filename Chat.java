056638d474dfd4605882e58e8bfb52215b68ed4f
Trevor Marsh <tcmarsh@users.noreply.github.com>
2017-10-06 00:37:10 -0600

    Chat specific comments
    Note that there's not really a good use for a friend in this context - again, that's something that would only serve to complicate this code.
    One of the OOP principles that applies here is that you shouldn't build something until you need it - in the case of this Chat, since you don't need more than two students in chat, you would probably be better off setting the students up just as a couple of instance variables instead of complicating the code with (possibly unnecessary) code to work with a variable length array

--- Chat.java
+++ Chat.java
@@ -4,8 +4,11 @@ public class Chat implements Comparable<Chat>{
 		
 	private int roomSize;
 	private int curSize;
+	// Chat links to Student which links back to Chat - this is a violation of the Single Responsibility Principle, and overall doesn't even seem to serve much of a purpose
 	private Student[] students;
-	private LinkedList<ChatEvent> events;
+	// You've put the chat response into the Chat instead of into the Student: -10 pts (doesn't match project requirements)
+	// Chat responses are not a unique collection by Student: -5 pts (non-unique, collection and not by student are points lost elsewhere)
+        private LinkedList<ChatEvent> events;
 
 	public Chat(int roomSize) {
 		this.roomSize = roomSize;
@@ -14,6 +17,7 @@ public class Chat implements Comparable<Chat>{
 		this.curSize = 0;
 	};
 	
+	// That comment's not very useful - I see you're wondering about friends, the concept doesn't exist in Java
 	//Where the heck are my C++ "friends"? This sucks.
 	protected void addStudent(Student newStudent) throws Error {
 		if (curSize < roomSize) {
@@ -24,6 +28,10 @@ public class Chat implements Comparable<Chat>{
 		}
 	}
 	
+	// The C++ method to manage an array used is very unfriendly and prone to errors - you'd be far better off
+	// (if you have to implement something with the array) doing other controls, as ArrayList -> Iterator and similar.
+	// USE THE CONSTRUCTS of the language or paradigm you're working with, Java is not simply "another way to write C++" or similar - it's a different language, with different strengths and weaknesses.
+	// One of the things you should strive to learn about any language you're learning is how the structures and algorithms you're already used to apply in the language you're working with.
 	protected void removeStudent(Student oldStudent) {
 		for (int i = 0; i < this.roomSize; i++) {
 			if (students[i] == oldStudent) {
@@ -50,6 +58,7 @@ public class Chat implements Comparable<Chat>{
 		return dialog;
 	}
 	
+	// You're not doing null checks in your compareTo method, how do you know that there are exactly the correct number of students in a chat?
 	public int compareTo(Chat other) {
 		return this.students[0].toString().compareTo(other.students[0].toString());
 	}
